a47b975b655b64d7ab923b403acdebb07836bbbc
MikhaelGK <mikhaelkiswanto8307@gmail.com>
2021-11-01 08:49:00 +0700

    Add SeedData

--- NSCMovie/Data/NSCMovieDbContext.cs
+++ NSCMovie/Data/NSCMovieDbContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using NSCMovie.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace NSCMovie.Data
 {
@@ -12,5 +13,26 @@ namespace NSCMovie.Data
        public DbSet<TranksaksiMovie> TranksaksiMovies { get; set; }
        public DbSet<Transaction> Transactions { get; set; }
        public DbSet<Rank> Ranks { get; set; }
+       protected override void OnModelCreating(ModelBuilder builder)
+       {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityRole>()
+                .HasData(new [] {
+                    new IdentityRole("Admin")
+                });
+            
+            var hasher = new PasswordHasher<Buyer>();
+            var admin = new Buyer{
+                UserName = "AdminWibu",
+                NormalizedUserName = "ADMINWIBU",
+                Email = "AdminWibu@admin.com",
+                NormalizedEmail = "ADMINWIBU@ADMIN.COM",
+                EmailConfirmed = true
+            };
+
+            admin.PasswordHash = hasher.HashPassword(admin, "12345678");
+            builder.Entity<Buyer>()
+                .HasData(admin);
+       }
     }
 }
