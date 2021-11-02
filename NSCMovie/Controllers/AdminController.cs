7a74de8beb9c99d154619406a10689209d61a7fb
Davina-Aulia-Putri <definap9@gmail.com>
2021-11-02 08:16:15 +0700

    Add AdminController

---
+++ NSCMovie/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NSCMovie.Controllers
+{
+    public class AdminController : Controller
+    {
+         
+    }
+  
+}
